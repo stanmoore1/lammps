89777910451d127725a5eb91a2b655189f1eee17
Stan Gerald Moore <stamoor@sandia.gov>
2020-11-11 15:19:33 -0700

    Fix Kokkos bug

--- src/KOKKOS/pair_reaxc_kokkos.cpp
+++ src/KOKKOS/pair_reaxc_kokkos.cpp
@@ -1100,7 +1100,7 @@ void PairReaxCKokkos<DeviceType>::operator()(PairReaxComputePolar<NEIGHFLAG,EVFL
   F_FLOAT epol = KCALpMOL_to_EV*(chi*qi+(eta/2.0)*qi*qi);
 
   /* energy due to coupling with kinetic energy potential */
-  if (system->acks2_flag)
+  if (acks2_flag)
     epol += KCALpMOL_to_EV*qi*d_s[NN + i];
 
   if (eflag) ev.ecoul += epol;
