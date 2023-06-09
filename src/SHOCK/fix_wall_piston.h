f655c0128f41360b0b59723cfc986808444aa954
Stan Gerald Moore <stamoor@sandia.gov>
2023-06-09 12:58:42 -0600

    Add override keyword

--- src/SHOCK/fix_wall_piston.h
+++ src/SHOCK/fix_wall_piston.h
@@ -27,15 +27,15 @@ namespace LAMMPS_NS {
 class FixWallPiston : public Fix {
  public:
   FixWallPiston(class LAMMPS *, int, char **);
-  int setmask();
-  void init();
-  void setup(int);
-  void min_setup(int);
-  void post_force(int);
-  void post_force_respa(int, int, int);
-  void min_post_force(int);
-  double compute_scalar();
-  double compute_vector(int);
+  int setmask() override;
+  void init() override;
+  void setup(int) override;
+  void min_setup(int) override;
+  void post_force(int) override;
+  void post_force_respa(int, int, int) override;
+  void min_post_force(int) override;
+  double compute_scalar() override;
+  double compute_vector(int) override;
 
  private:
   int dim,side;
