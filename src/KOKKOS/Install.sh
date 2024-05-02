56b89ac41edf1437611bd74987ec36503618b8c7
Lewis Russell <lewisrussell222@gmail.com>
2024-05-02 23:32:02 +0100

    Updated for oxdna/fene/kk files

--- src/KOKKOS/Install.sh
+++ src/KOKKOS/Install.sh
@@ -90,6 +90,8 @@ action bond_harmonic_kokkos.cpp bond_harmonic.cpp
 action bond_harmonic_kokkos.h bond_harmonic.h
 action bond_oxdna_fene_kokkos.cpp bond_oxdna_fene.cpp
 action bond_oxdna_fene_kokkos.h bond_oxdna_fene.h
+action bond_oxdna2_fene_kokkos.h bond_oxdna_fene_kokkos.h
+action bond_oxrna2_fene_kokkos.h bond_oxdna_fene_kokkos.h
 action comm_kokkos.cpp
 action comm_kokkos.h
 action comm_tiled_kokkos.cpp
