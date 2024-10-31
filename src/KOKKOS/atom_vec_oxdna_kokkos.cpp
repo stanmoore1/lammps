6947b127c80cc24e56f0e8b884edf20f95b319f9
Lewis Russell <lewis.russell@strath.ac.uk>
2024-10-31 16:15:05 +0000

    Initial Development of avec_oxdnaKK

--- src/KOKKOS/atom_vec_oxdna_kokkos.cpp
+++ src/KOKKOS/atom_vec_oxdna_kokkos.cpp
@@ -11,7 +11,7 @@
 
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
-
+//TODO: Check "BOND_MASK" is the correct MASK for oxdnaKK
 #include "atom_vec_oxdna_kokkos.h"
 
 #include "atom_kokkos.h"
@@ -52,34 +52,7 @@ void AtomVecOxdnaKokkos::grow(int n)
   atomKK->sync(Device,ALL_MASK);
   atomKK->modified(Device,ALL_MASK);
 
-  memoryKK->grow_kokkos(atomKK->k_tag,atomKK->tag,nmax,"atom:tag");
-  memoryKK->grow_kokkos(atomKK->k_type,atomKK->type,nmax,"atom:type");
-  memoryKK->grow_kokkos(atomKK->k_mask,atomKK->mask,nmax,"atom:mask");
-  memoryKK->grow_kokkos(atomKK->k_image,atomKK->image,nmax,"atom:image");
-
-  memoryKK->grow_kokkos(atomKK->k_x,atomKK->x,nmax,"atom:x");
-  memoryKK->grow_kokkos(atomKK->k_v,atomKK->v,nmax,"atom:v");
-  memoryKK->grow_kokkos(atomKK->k_f,atomKK->f,nmax,"atom:f");
-
-  memoryKK->grow_kokkos(atomKK->k_molecule,atomKK->molecule,nmax,"atom:molecule");
-  memoryKK->grow_kokkos(atomKK->k_nspecial,atomKK->nspecial,nmax,3,"atom:nspecial");
-  memoryKK->grow_kokkos(atomKK->k_special,atomKK->special,nmax,atomKK->maxspecial,
-                      "atom:special");
-  memoryKK->grow_kokkos(atomKK->k_num_bond,atomKK->num_bond,nmax,"atom:num_bond");
-  memoryKK->grow_kokkos(atomKK->k_bond_type,atomKK->bond_type,nmax,atomKK->bond_per_atom,
-                      "atom:bond_type");
-  memoryKK->grow_kokkos(atomKK->k_bond_atom,atomKK->bond_atom,nmax,atomKK->bond_per_atom,
-                      "atom:bond_atom");
-
-  memoryKK->grow_kokkos(atomKK->k_num_angle,atomKK->num_angle,nmax,"atom:num_angle");
-  memoryKK->grow_kokkos(atomKK->k_angle_type,atomKK->angle_type,nmax,atomKK->angle_per_atom,
-                      "atom:angle_type");
-  memoryKK->grow_kokkos(atomKK->k_angle_atom1,atomKK->angle_atom1,nmax,atomKK->angle_per_atom,
-                      "atom:angle_atom1");
-  memoryKK->grow_kokkos(atomKK->k_angle_atom2,atomKK->angle_atom2,nmax,atomKK->angle_per_atom,
-                      "atom:angle_atom2");
-  memoryKK->grow_kokkos(atomKK->k_angle_atom3,atomKK->angle_atom3,nmax,atomKK->angle_per_atom,
-                      "atom:angle_atom3");
+  memoryKK->grow_kokkos(atomKK->k_id5p,atomKK->id5p,nmax,"atom:id5p");
 
   grow_pointers();
   atomKK->sync(Host,ALL_MASK);
@@ -95,64 +68,10 @@ void AtomVecOxdnaKokkos::grow(int n)
 
 void AtomVecOxdnaKokkos::grow_pointers()
 {
-  tag = atomKK->tag;
-  d_tag = atomKK->k_tag.d_view;
-  h_tag = atomKK->k_tag.h_view;
+  id5p = atomKK->id5p;
+  d_id5p = atomKK->k_id5p.d_view;
+  h_id5p = atomKK->k_id5p.h_view;
 
-  type = atomKK->type;
-  d_type = atomKK->k_type.d_view;
-  h_type = atomKK->k_type.h_view;
-  mask = atomKK->mask;
-  d_mask = atomKK->k_mask.d_view;
-  h_mask = atomKK->k_mask.h_view;
-  image = atomKK->image;
-  d_image = atomKK->k_image.d_view;
-  h_image = atomKK->k_image.h_view;
-
-  x = atomKK->x;
-  d_x = atomKK->k_x.d_view;
-  h_x = atomKK->k_x.h_view;
-  v = atomKK->v;
-  d_v = atomKK->k_v.d_view;
-  h_v = atomKK->k_v.h_view;
-  f = atomKK->f;
-  d_f = atomKK->k_f.d_view;
-  h_f = atomKK->k_f.h_view;
-
-  molecule = atomKK->molecule;
-  d_molecule = atomKK->k_molecule.d_view;
-  h_molecule = atomKK->k_molecule.h_view;
-  nspecial = atomKK->nspecial;
-  d_nspecial = atomKK->k_nspecial.d_view;
-  h_nspecial = atomKK->k_nspecial.h_view;
-  special = atomKK->special;
-  d_special = atomKK->k_special.d_view;
-  h_special = atomKK->k_special.h_view;
-  num_bond = atomKK->num_bond;
-  d_num_bond = atomKK->k_num_bond.d_view;
-  h_num_bond = atomKK->k_num_bond.h_view;
-  bond_type = atomKK->bond_type;
-  d_bond_type = atomKK->k_bond_type.d_view;
-  h_bond_type = atomKK->k_bond_type.h_view;
-  bond_atom = atomKK->bond_atom;
-  d_bond_atom = atomKK->k_bond_atom.d_view;
-  h_bond_atom = atomKK->k_bond_atom.h_view;
-
-  num_angle = atomKK->num_angle;
-  d_num_angle = atomKK->k_num_angle.d_view;
-  h_num_angle = atomKK->k_num_angle.h_view;
-  angle_type = atomKK->angle_type;
-  d_angle_type = atomKK->k_angle_type.d_view;
-  h_angle_type = atomKK->k_angle_type.h_view;
-  angle_atom1 = atomKK->angle_atom1;
-  d_angle_atom1 = atomKK->k_angle_atom1.d_view;
-  h_angle_atom1 = atomKK->k_angle_atom1.h_view;
-  angle_atom2 = atomKK->angle_atom2;
-  d_angle_atom2 = atomKK->k_angle_atom2.d_view;
-  h_angle_atom2 = atomKK->k_angle_atom2.h_view;
-  angle_atom3 = atomKK->angle_atom3;
-  d_angle_atom3 = atomKK->k_angle_atom3.d_view;
-  h_angle_atom3 = atomKK->k_angle_atom3.h_view;
 }
 
 /* ----------------------------------------------------------------------
@@ -163,23 +82,7 @@ void AtomVecOxdnaKokkos::sort_kokkos(Kokkos::BinSort<KeyViewType, BinOp> &Sorter
 {
   atomKK->sync(Device, ALL_MASK & ~F_MASK);
 
-  Sorter.sort(LMPDeviceType(), d_tag);
-  Sorter.sort(LMPDeviceType(), d_type);
-  Sorter.sort(LMPDeviceType(), d_mask);
-  Sorter.sort(LMPDeviceType(), d_image);
-  Sorter.sort(LMPDeviceType(), d_x);
-  Sorter.sort(LMPDeviceType(), d_v);
-  Sorter.sort(LMPDeviceType(), d_molecule);
-  Sorter.sort(LMPDeviceType(), d_num_bond);
-  Sorter.sort(LMPDeviceType(), d_bond_type);
-  Sorter.sort(LMPDeviceType(), d_bond_atom);
-  Sorter.sort(LMPDeviceType(), d_nspecial);
-  Sorter.sort(LMPDeviceType(), d_special);
-  Sorter.sort(LMPDeviceType(), d_num_angle);
-  Sorter.sort(LMPDeviceType(), d_angle_type);
-  Sorter.sort(LMPDeviceType(), d_angle_atom1);
-  Sorter.sort(LMPDeviceType(), d_angle_atom2);
-  Sorter.sort(LMPDeviceType(), d_angle_atom3);
+  Sorter.sort(LMPDeviceType(), d_id5p);
 
   atomKK->modified(Device, ALL_MASK & ~F_MASK);
 }
@@ -189,55 +92,9 @@ void AtomVecOxdnaKokkos::sort_kokkos(Kokkos::BinSort<KeyViewType, BinOp> &Sorter
 void AtomVecOxdnaKokkos::sync(ExecutionSpace space, unsigned int mask)
 {
   if (space == Device) {
-    if (mask & X_MASK) atomKK->k_x.sync<LMPDeviceType>();
-    if (mask & V_MASK) atomKK->k_v.sync<LMPDeviceType>();
-    if (mask & F_MASK) atomKK->k_f.sync<LMPDeviceType>();
-    if (mask & TAG_MASK) atomKK->k_tag.sync<LMPDeviceType>();
-    if (mask & TYPE_MASK) atomKK->k_type.sync<LMPDeviceType>();
-    if (mask & MASK_MASK) atomKK->k_mask.sync<LMPDeviceType>();
-    if (mask & IMAGE_MASK) atomKK->k_image.sync<LMPDeviceType>();
-    if (mask & MOLECULE_MASK) atomKK->k_molecule.sync<LMPDeviceType>();
-    if (mask & SPECIAL_MASK) {
-      atomKK->k_nspecial.sync<LMPDeviceType>();
-      atomKK->k_special.sync<LMPDeviceType>();
-    }
-    if (mask & BOND_MASK) {
-      atomKK->k_num_bond.sync<LMPDeviceType>();
-      atomKK->k_bond_type.sync<LMPDeviceType>();
-      atomKK->k_bond_atom.sync<LMPDeviceType>();
-    }
-    if (mask & ANGLE_MASK) {
-      atomKK->k_num_angle.sync<LMPDeviceType>();
-      atomKK->k_angle_type.sync<LMPDeviceType>();
-      atomKK->k_angle_atom1.sync<LMPDeviceType>();
-      atomKK->k_angle_atom2.sync<LMPDeviceType>();
-      atomKK->k_angle_atom3.sync<LMPDeviceType>();
-    }
+    if (mask & BOND_MASK) atomKK->k_id5p.sync<LMPDeviceType>();
   } else {
-    if (mask & X_MASK) atomKK->k_x.sync<LMPHostType>();
-    if (mask & V_MASK) atomKK->k_v.sync<LMPHostType>();
-    if (mask & F_MASK) atomKK->k_f.sync<LMPHostType>();
-    if (mask & TAG_MASK) atomKK->k_tag.sync<LMPHostType>();
-    if (mask & TYPE_MASK) atomKK->k_type.sync<LMPHostType>();
-    if (mask & MASK_MASK) atomKK->k_mask.sync<LMPHostType>();
-    if (mask & IMAGE_MASK) atomKK->k_image.sync<LMPHostType>();
-    if (mask & MOLECULE_MASK) atomKK->k_molecule.sync<LMPHostType>();
-    if (mask & SPECIAL_MASK) {
-      atomKK->k_nspecial.sync<LMPHostType>();
-      atomKK->k_special.sync<LMPHostType>();
-    }
-    if (mask & BOND_MASK) {
-      atomKK->k_num_bond.sync<LMPHostType>();
-      atomKK->k_bond_type.sync<LMPHostType>();
-      atomKK->k_bond_atom.sync<LMPHostType>();
-    }
-    if (mask & ANGLE_MASK) {
-      atomKK->k_num_angle.sync<LMPHostType>();
-      atomKK->k_angle_type.sync<LMPHostType>();
-      atomKK->k_angle_atom1.sync<LMPHostType>();
-      atomKK->k_angle_atom2.sync<LMPHostType>();
-      atomKK->k_angle_atom3.sync<LMPHostType>();
-    }
+    if (mask & BOND_MASK) atomKK->k_id5p.sync<LMPHostType>();
   }
 }
 
@@ -246,91 +103,11 @@ void AtomVecOxdnaKokkos::sync(ExecutionSpace space, unsigned int mask)
 void AtomVecOxdnaKokkos::sync_overlapping_device(ExecutionSpace space, unsigned int mask)
 {
   if (space == Device) {
-    if ((mask & X_MASK) && atomKK->k_x.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_x_array>(atomKK->k_x,space);
-    if ((mask & V_MASK) && atomKK->k_v.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_v_array>(atomKK->k_v,space);
-    if ((mask & F_MASK) && atomKK->k_f.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_f_array>(atomKK->k_f,space);
-    if ((mask & TAG_MASK) && atomKK->k_tag.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_tagint_1d>(atomKK->k_tag,space);
-    if ((mask & TYPE_MASK) && atomKK->k_type.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_int_1d>(atomKK->k_type,space);
-    if ((mask & MASK_MASK) && atomKK->k_mask.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_int_1d>(atomKK->k_mask,space);
-    if ((mask & IMAGE_MASK) && atomKK->k_image.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_imageint_1d>(atomKK->k_image,space);
-    if ((mask & MOLECULE_MASK) && atomKK->k_molecule.need_sync<LMPDeviceType>())
-      perform_async_copy<DAT::tdual_tagint_1d>(atomKK->k_molecule,space);
-    if (mask & SPECIAL_MASK) {
-      if (atomKK->k_nspecial.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_int_2d>(atomKK->k_nspecial,space);
-      if (atomKK->k_special.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_special,space);
-    }
-    if (mask & BOND_MASK) {
-      if (atomKK->k_num_bond.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_int_1d>(atomKK->k_num_bond,space);
-      if (atomKK->k_bond_type.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_int_2d>(atomKK->k_bond_type,space);
-      if (atomKK->k_bond_atom.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_bond_atom,space);
-    }
-    if (mask & ANGLE_MASK) {
-      if (atomKK->k_num_angle.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_int_1d>(atomKK->k_num_angle,space);
-      if (atomKK->k_angle_type.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_int_2d>(atomKK->k_angle_type,space);
-      if (atomKK->k_angle_atom1.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_angle_atom1,space);
-      if (atomKK->k_angle_atom2.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_angle_atom2,space);
-      if (atomKK->k_angle_atom3.need_sync<LMPDeviceType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_angle_atom3,space);
-    }
+    if ((mask & BOND_MASK) && atomKK->k_id5p.need_sync<LMPDeviceType>())
+      perform_async_copy<DAT::tdual_tagint_1d>(atomKK->k_id5p,space);
   } else {
-    if ((mask & X_MASK) && atomKK->k_x.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_x_array>(atomKK->k_x,space);
-    if ((mask & V_MASK) && atomKK->k_v.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_v_array>(atomKK->k_v,space);
-    if ((mask & F_MASK) && atomKK->k_f.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_f_array>(atomKK->k_f,space);
-    if ((mask & TAG_MASK) && atomKK->k_tag.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_tagint_1d>(atomKK->k_tag,space);
-    if ((mask & TYPE_MASK) && atomKK->k_type.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_int_1d>(atomKK->k_type,space);
-    if ((mask & MASK_MASK) && atomKK->k_mask.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_int_1d>(atomKK->k_mask,space);
-    if ((mask & IMAGE_MASK) && atomKK->k_image.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_imageint_1d>(atomKK->k_image,space);
-    if ((mask & MOLECULE_MASK) && atomKK->k_molecule.need_sync<LMPHostType>())
-      perform_async_copy<DAT::tdual_tagint_1d>(atomKK->k_molecule,space);
-    if (mask & SPECIAL_MASK) {
-      if (atomKK->k_nspecial.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_int_2d>(atomKK->k_nspecial,space);
-      if (atomKK->k_special.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_special,space);
-    }
-    if (mask & BOND_MASK) {
-      if (atomKK->k_num_bond.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_int_1d>(atomKK->k_num_bond,space);
-      if (atomKK->k_bond_type.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_int_2d>(atomKK->k_bond_type,space);
-      if (atomKK->k_bond_atom.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_bond_atom,space);
-    }
-    if (mask & ANGLE_MASK) {
-      if (atomKK->k_num_angle.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_int_1d>(atomKK->k_num_angle,space);
-      if (atomKK->k_angle_type.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_int_2d>(atomKK->k_angle_type,space);
-      if (atomKK->k_angle_atom1.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_angle_atom1,space);
-      if (atomKK->k_angle_atom2.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_angle_atom2,space);
-      if (atomKK->k_angle_atom3.need_sync<LMPHostType>())
-        perform_async_copy<DAT::tdual_tagint_2d>(atomKK->k_angle_atom3,space);
-    }
+    if ((mask & BOND_MASK) && atomKK->k_id5p.need_sync<LMPHostType>())
+      perform_async_copy<DAT::tdual_tagint_1d>(atomKK->k_id5p,space);
   }
 }
 
@@ -339,54 +116,8 @@ void AtomVecOxdnaKokkos::sync_overlapping_device(ExecutionSpace space, unsigned 
 void AtomVecOxdnaKokkos::modified(ExecutionSpace space, unsigned int mask)
 {
   if (space == Device) {
-    if (mask & X_MASK) atomKK->k_x.modify<LMPDeviceType>();
-    if (mask & V_MASK) atomKK->k_v.modify<LMPDeviceType>();
-    if (mask & F_MASK) atomKK->k_f.modify<LMPDeviceType>();
-    if (mask & TAG_MASK) atomKK->k_tag.modify<LMPDeviceType>();
-    if (mask & TYPE_MASK) atomKK->k_type.modify<LMPDeviceType>();
-    if (mask & MASK_MASK) atomKK->k_mask.modify<LMPDeviceType>();
-    if (mask & IMAGE_MASK) atomKK->k_image.modify<LMPDeviceType>();
-    if (mask & MOLECULE_MASK) atomKK->k_molecule.modify<LMPDeviceType>();
-    if (mask & SPECIAL_MASK) {
-      atomKK->k_nspecial.modify<LMPDeviceType>();
-      atomKK->k_special.modify<LMPDeviceType>();
-    }
-    if (mask & BOND_MASK) {
-      atomKK->k_num_bond.modify<LMPDeviceType>();
-      atomKK->k_bond_type.modify<LMPDeviceType>();
-      atomKK->k_bond_atom.modify<LMPDeviceType>();
-    }
-    if (mask & ANGLE_MASK) {
-      atomKK->k_num_angle.modify<LMPDeviceType>();
-      atomKK->k_angle_type.modify<LMPDeviceType>();
-      atomKK->k_angle_atom1.modify<LMPDeviceType>();
-      atomKK->k_angle_atom2.modify<LMPDeviceType>();
-      atomKK->k_angle_atom3.modify<LMPDeviceType>();
-    }
+    if (mask & BOND_MASK) atomKK->k_id5p.modify<LMPDeviceType>();
   } else {
-    if (mask & X_MASK) atomKK->k_x.modify<LMPHostType>();
-    if (mask & V_MASK) atomKK->k_v.modify<LMPHostType>();
-    if (mask & F_MASK) atomKK->k_f.modify<LMPHostType>();
-    if (mask & TAG_MASK) atomKK->k_tag.modify<LMPHostType>();
-    if (mask & TYPE_MASK) atomKK->k_type.modify<LMPHostType>();
-    if (mask & MASK_MASK) atomKK->k_mask.modify<LMPHostType>();
-    if (mask & IMAGE_MASK) atomKK->k_image.modify<LMPHostType>();
-    if (mask & MOLECULE_MASK) atomKK->k_molecule.modify<LMPHostType>();
-    if (mask & SPECIAL_MASK) {
-      atomKK->k_nspecial.modify<LMPHostType>();
-      atomKK->k_special.modify<LMPHostType>();
-    }
-    if (mask & BOND_MASK) {
-      atomKK->k_num_bond.modify<LMPHostType>();
-      atomKK->k_bond_type.modify<LMPHostType>();
-      atomKK->k_bond_atom.modify<LMPHostType>();
-    }
-    if (mask & ANGLE_MASK) {
-      atomKK->k_num_angle.modify<LMPHostType>();
-      atomKK->k_angle_type.modify<LMPHostType>();
-      atomKK->k_angle_atom1.modify<LMPHostType>();
-      atomKK->k_angle_atom2.modify<LMPHostType>();
-      atomKK->k_angle_atom3.modify<LMPHostType>();
-    }
+    if (mask & BOND_MASK) atomKK->k_id5p.modify<LMPHostType>();
   }
 }
