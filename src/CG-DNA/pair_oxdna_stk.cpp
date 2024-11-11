b582e5d9b456ca4ee126aa30c968974d1c1a4f9b
Lewis Russell <lewis.russell@strath.ac.uk>
2024-11-11 17:38:38 +0000

    copymode added for KOKKOS support

--- src/CG-DNA/pair_oxdna_stk.cpp
+++ src/CG-DNA/pair_oxdna_stk.cpp
@@ -72,7 +72,7 @@ PairOxdnaStk::PairOxdnaStk(LAMMPS *lmp) : Pair(lmp)
 
 PairOxdnaStk::~PairOxdnaStk()
 {
-  if (allocated) {
+  if (allocated && !copymode) {
 
     memory->destroy(setflag);
     memory->destroy(cutsq);
