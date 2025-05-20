521d2b9a1615b054e95203a070bbf6c68b0f983c
alphataubio <alphataubio@gmail.com>
2025-05-20 14:42:04 -0400

    add typename and using

--- src/KOKKOS/kokkos_type.h
+++ src/KOKKOS/kokkos_type.h
@@ -767,13 +767,27 @@ struct TripleView {
 
 // --------------------------------------------------------------------------------
 
+// For device views with fully qualified types
 #define KOKKOS_DEVICE_DUALVIEW(TYPE, LAYOUT, SUFFIX) \
 typedef Kokkos::DualView<TYPE, LAYOUT, LMPDeviceType> tdual_##SUFFIX; \
-typedef tdual_##SUFFIX::t_dev t_##SUFFIX; \
-typedef tdual_##SUFFIX::t_dev_const t_##SUFFIX##_const; \
-typedef tdual_##SUFFIX::t_dev_um t_##SUFFIX##_um; \
-typedef tdual_##SUFFIX::t_dev_const_um t_##SUFFIX##_const_um; \
-typedef tdual_##SUFFIX::t_dev_const_randomread t_##SUFFIX##_randomread;
+typedef typename tdual_##SUFFIX::t_dev t_##SUFFIX; \
+typedef typename tdual_##SUFFIX::t_dev_const t_##SUFFIX##_const; \
+typedef typename tdual_##SUFFIX::t_dev_um t_##SUFFIX##_um; \
+typedef typename tdual_##SUFFIX::t_dev_const_um t_##SUFFIX##_const_um; \
+typedef typename tdual_##SUFFIX::t_dev_const_randomread t_##SUFFIX##_randomread;
+
+// For host views with fully qualified types
+#define KOKKOS_HOST_DUALVIEW(TYPE, LAYOUT, SUFFIX) \
+typedef Kokkos::DualView<TYPE, LAYOUT, LMPDeviceType> tdual_##SUFFIX; \
+typedef typename tdual_##SUFFIX::t_host t_##SUFFIX; \
+typedef typename tdual_##SUFFIX::t_host_const t_##SUFFIX##_const; \
+typedef typename tdual_##SUFFIX::t_host_um t_##SUFFIX##_um; \
+typedef typename tdual_##SUFFIX::t_host_const_um t_##SUFFIX##_const_um; \
+typedef typename tdual_##SUFFIX::t_host_const_randomread t_##SUFFIX##_randomread;
+
+using LAMMPS_NS::bigint;
+using LAMMPS_NS::tagint;
+using LAMMPS_NS::imageint;
 
 template <class DeviceType>
 struct ArrayTypes;
@@ -856,14 +870,6 @@ typedef tdual_neighbors_2d_lr::t_dev_const_um t_neighbors_2d_const_um_lr;
 typedef tdual_neighbors_2d_lr::t_dev_const_randomread t_neighbors_2d_randomread_lr;
 
 };
-
-#define KOKKOS_HOST_DUALVIEW(TYPE, LAYOUT, SUFFIX) \
-typedef Kokkos::DualView<TYPE, LAYOUT, LMPDeviceType> tdual_##SUFFIX; \
-typedef tdual_##SUFFIX::t_host t_##SUFFIX; \
-typedef tdual_##SUFFIX::t_host_const t_##SUFFIX##_const; \
-typedef tdual_##SUFFIX::t_host_um t_##SUFFIX##_um; \
-typedef tdual_##SUFFIX::t_host_const_um t_##SUFFIX##_const_um; \
-typedef tdual_##SUFFIX::t_host_const_randomread t_##SUFFIX##_randomread;
 
 #ifdef LMP_KOKKOS_GPU
 template <>
