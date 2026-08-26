f651957dab451e1a9d720f631ab4c01a8b6e8073
Axel Kohlmeyer <akohlmey@gmail.com>
2026-08-26 11:21:47 -0400

    cosmetic

--- src/lammps.cpp
+++ src/lammps.cpp
@@ -126,6 +126,7 @@ LAMMPS::LAMMPS(int narg, char **arg, MPI_Comm communicator) :
   // idempotent and thread-safe; runs only on the first LAMMPS instance.
   register_builtin_styles();
 
+  // copy arguments for use with FENIX package
   num_in_arg = narg;
   in_args = num_in_arg ? new char*[num_in_arg] : nullptr;
   for (int i = 0; i < num_in_arg; i++) {
